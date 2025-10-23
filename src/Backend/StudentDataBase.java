c7c926674a482fb3f385000f0d6ae87dde112924
Youssef Fathy <yousseffathy771@gmail.com>
2025-10-23 20:39:15 +0300

    new class added,StudentAdd to file ((((Check for errors))))

--- src/Backend/StudentDataBase.java
+++ src/Backend/StudentDataBase.java
@@ -3,11 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Backend;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 /**
  *
  * @author mo
  */
 public class StudentDataBase {
+     
     
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String DP; 
+    private double gpa;
+ 
+    
+    public StudentDataBase (int id, String name, int age, String gender, String DP, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.DP = DP;
+        this.gpa = gpa;
+    }
+
+
+    public int getID() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getGender() { return gender; }
+    public String getDepartment() { return DP; }
+    public double getGPA() { return gpa; }
+
+    
+    public String Save() {
+        return id + "," + name + "," + age + "," + gender + "," + DP + "," + gpa  ;
+    }
 }
+
+
