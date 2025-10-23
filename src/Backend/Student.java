ed32a1289262208a211f3d6cec2c0a78c9c4bf74
Youssef Fathy <yousseffathy771@gmail.com>
2025-10-23 20:41:11 +0300

    new class added, StudentAdd ((((Check for errors))))

--- src/Backend/Student.java
+++ src/Backend/Student.java
@@ -8,6 +8,55 @@ package Backend;
  *
  * @author mo
  */
+
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
 public class Student {
     
+    private static final String FILENAME = "student.txt";
+
+    public static void addStudent(StudentDataBase student) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME, true))) {
+            writer.write(student.Save());
+            writer.newLine();
+        } 
+    }
+    public static ArrayList<StudentDataBase> loadStudentsFromFile() throws IOException {
+        ArrayList<StudentDataBase> students = new ArrayList<>();
+         try (FileReader fileReader = new FileReader(FILENAME);
+        BufferedReader bufferedReader = new BufferedReader(fileReader)){
+        
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] parts = line.split(",");
+            
+            if (parts.length == 6) {
+                
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                int age = Integer.parseInt(parts[2]);
+                String gender = parts[3];
+                String DP = parts[4];
+                double gpa = Double.parseDouble(parts[5]);
+                
+                
+                students.add(new StudentDataBase(id, name, age, gender, DP, gpa));
+            }
+        }
+        
+    }
+       
+        
+        
+        return students;
+    }
 }
