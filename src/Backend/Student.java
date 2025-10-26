6b8580f8beb30cbc6a37930eb1768997e6580fa1
abdelrahman <abdoelsayedmo2005@gmail.com>
2025-10-26 13:11:26 +0300

    editing name

--- src/Backend/Student.java
+++ src/Backend/Student.java
@@ -4,59 +4,84 @@
  */
 package Backend;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 /**
  *
  * @author mo
- */
-
-
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-
-
-
+ */1
 public class Student {
-    
-    private static final String FILENAME = "student.txt";
 
-    public static void addStudent(StudentDataBase student) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME, true))) {
-            writer.write(student.Save());
-            writer.newLine();
-        } 
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String DP;
+    private double gpa;
+
+    public Student(int id, String name, int age, String gender, String DP, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.DP = DP;
+        this.gpa = gpa;
     }
-    public static ArrayList<StudentDataBase> loadStudentsFromFile() throws IOException {
-        ArrayList<StudentDataBase> students = new ArrayList<>();
-         try (FileReader fileReader = new FileReader(FILENAME);
-        BufferedReader bufferedReader = new BufferedReader(fileReader)){
-        
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            String[] parts = line.split(",");
-            
-            if (parts.length == 6) {
-                
-                int id = Integer.parseInt(parts[0]);
-                String name = parts[1];
-                int age = Integer.parseInt(parts[2]);
-                String gender = parts[3];
-                String DP = parts[4];
-                double gpa = Double.parseDouble(parts[5]);
-                
-                
-                students.add(new StudentDataBase(id, name, age, gender, DP, gpa));
-            }
-        }
-        
+
+    public int getID() {
+        return id;
     }
-       
-        
-        
-        return students;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getDepartment() {
+        return DP;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDP(String DP) {
+        this.DP = DP;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGPA() {
+        return gpa;
+    }
+
+    public String Save() {
+        return id + "," + name + "," + age + "," + gender + "," + DP + "," + gpa;
     }
 }
